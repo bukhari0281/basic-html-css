93166e1afdd012c7f09e308417287d981767cb0e
Al Bukhari Bin Riedho <bukhari0281@gmail.com>
2024-02-13 11:08:32 +0700

    .

--- tugas-akhir/assets/js/script.js
+++ tugas-akhir/assets/js/script.js
@@ -1,11 +1,11 @@
-const menu_bar = document.querySelector(".menu-bar");
+// const menu_bar = document.querySelector(".menu-bar");
 const menu_nav = document.querySelector(".navbar-nav");
 
 document.querySelector("#menu-bar").onclick = () => {
     menu_nav.classList.toggle("active");
 }
 
-menu_bar.addEventListener("click", function() {
-    menu_nav.classList.toggle("menu-active");
-})
+// menu_bar.addEventListener("click", function() {
+//     menu_nav.classList.toggle("active");
+// })
 

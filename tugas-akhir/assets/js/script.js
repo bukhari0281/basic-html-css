11586b179a02a37678782af19ad3d86457d5c76f
Al Bukhari Bin Riedho <bukhari0281@gmail.com>
2024-02-13 09:16:47 +0700

    .

--- tugas-akhir/assets/js/script.js
+++ tugas-akhir/assets/js/script.js
@@ -1,5 +1,9 @@
 const menu_bar = document.querySelector(".menu-bar");
-const menu_nav = document.querySelector(".nav-menu");
+const menu_nav = document.querySelector(".navbar-nav");
+
+document.querySelector("#menu-bar").onclick = () => {
+    menu_nav.classList.toggle("active");
+}
 
 menu_bar.addEventListener("click", function() {
     menu_nav.classList.toggle("menu-active");
